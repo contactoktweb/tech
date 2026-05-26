39a292ad6bf2f9694e0d3591716f5ab138bdeb83
ktrillos2 <ktrillos2@gmail.com>
2026-05-26 09:26:48 -0500

    Fix Vercel build: Add fallback for NEXT_PUBLIC_SANITY_PROJECT_ID

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,7 +2,7 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-05-16'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '069iit0r'
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
